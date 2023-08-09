1d4f0846eacef8c979e5ac6ebadf68bca5493246
Angel <santoyox714@gmail.com>
2023-08-08 18:08:37 -0700

    add qr

---
+++ src/routes/(site)/qr/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { QRCodeImage } from "svelte-qrcode-image";
+    let links_url = "https://www.links.angs.dev/";
+</script>
+
+<div class="container">
+    <div class="qr-container">
+        <QRCodeImage text={links_url} width={600} height={600} />
+    </div>
+</div>
+
+<style>
+    :global(body) {
+        background: #2e3541;
+        font-family: Arial, sans-serif;
+        margin: 0;
+    }
+
+    .container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        min-height: 100vh;
+    }
+
+    .qr-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
